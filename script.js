168cd982bccb1fa9324d966fafa4d841dc09bfde
IsmaGold67 <ismaelroberto67@gmail.com>
2023-09-15 23:49:13 -0400

    first-commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const lightAndDark = document.querySelector('.lightAndDark')
+const headChat = document.querySelector('.head-chat')
+
+lightAndDark.addEventListener('click' = event => {
+    headChat.style.backgroundColor = '#000'
+
+    console.log(event)
+})
