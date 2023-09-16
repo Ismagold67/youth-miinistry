a31d87e36b54936f5f6ec54a627d2b2ed537b31a
IsmaGold67 <ismaelroberto67@gmail.com>
2023-09-16 13:44:30 -0400

    delet script extern

--- script.js
+++
@@ -1,8 +0,0 @@
-const lightAndDark = document.querySelector('.lightAndDark')
-const headChat = document.querySelector('.head-chat')
-
-lightAndDark.addEventListener('click' = event => {
-    headChat.style.backgroundColor = '#000'
-
-    console.log(event)
-})
